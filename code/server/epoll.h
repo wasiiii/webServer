8294a64c40070826d3ee2b8d8db1a3483f27593f
wasiii <chengwasi@163.com>
2021-06-20 17:57:43 -0700

    epoll

--- code/server/epoll.h
+++ code/server/epoll.h
@@ -1,5 +1,36 @@
 #ifndef EPOLL_H
 #define EPOLL_H
 
+#include <sys/epoll.h>  // epoll_ctl()
+#include <fcntl.h>      // fcntl()
+#include <unistd.h>     // close()
+#include <assert.h>     // close()
+#include <vector>
+#include <errno.h>
+
+class Epoller {
+public:
+    explicit Epoller(int maxEvent = 1024);
+
+    ~Epoller();
+
+    bool addFd(int fd, uint32_t events);
+
+    bool modFd(int fd, uint32_t events);
+
+    bool delFd(int fd);
+
+    // 默认-1，永远阻塞，直到有事件发生
+    int wait(int timeoutMs = -1);
+
+    int getEventFd(size_t index) const;
+
+    uint32_t getEvents(size_t index) const;
+
+private:
+    int epollFd_;
+
+    std::vector<struct epoll_event> events_;
+};
 
 #endif
